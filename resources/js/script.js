60ecc84e88262f357e8ab3cba33c4e1af7d7f7ae
Phu Phan <phu.phan@vinasource.com>
2017-04-03 17:11:29 +0700

    Using waypoint to fixed menu header when scroll

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,23 @@
 $(document).ready(function () {
-    $('h1').click(function () {
-        $(this).css('background-color', 'red');
+
+    $('.js--section-features').waypoint(function (direction) {
+        if (direction === 'down') {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px'
     });
+
+
+    /*
+
+var waypoints = $('#handler-first').waypoint(function(direction) {
+    notify(this.element.id + ' hit 25% from top of window')
+    },
+    {offset: '25%'}
+)
+
+     */
 });
